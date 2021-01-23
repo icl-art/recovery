5fdd8dd74425c232d59fdba43fae80f06fb3f5ca
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-01-23 12:32:01 +0000

    Adding Rohit's shock cord forces calculator

---
+++ shock_calc.m
@@ -0,0 +1,101 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+MATLAB script that calculates the time-varying load in the shock 
+chord using Newton's second law. The rocket is modelled as a point mass and
+the shock cord is inextensible. The variation of the parachute force as it 
+opens is modelled by a polynomial initial period followed by a constant 
+force, with the parachute opening time as a input.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear
+clc
+clear all
+
+  %   inputs
+Cd = 1.4;       %   parachute drag coefficient
+L = 2.5;        %   shock cord length
+S = pi*0.15^2;  %   parachute area
+m = 1.4;        %   rocket mass
+t_delay = 9;    %   parachute delployment delay (after apogee)
+
+  %   simulation time parameters
+dt = 0.01;
+total_time = 5;
+sim_time = 0;
+
+V0 = sqrt(2*9.81*L+t_delay^2*9.81^2);   
+v = [];
+v(1) = -V0;
+v(2) = V0+10;
+k = 0.5*1.225*Cd*S/m;
+grad = [];
+grad(1:2) = 0;
+
+error = 10;
+
+  %   parachute force scale factor
+F = ones(1,total_time/dt+3);
+F_open = 0.2;     %   time taken to open parachute fully
+
+F_ticks = F_open/dt;
+
+disp('Assume parachute opening can be modelled by an nth order polynomial')
+n = input('n = ');
+
+if n ~= 0
+    
+    for i = 1:F_ticks
+   
+        F(i) = (i*dt)^n/(F_open)^n;
+    
+    end
+end
+
+  % plot to show parachute force scaling
+figure
+plot(F)
+ylim([0 1.2])
+xlim([0 length(F)/10])
+grid on
+box on
+
+i = 2;
+
+  %   solver loop
+while sim_time<total_time+dt
+    v(i) = (k*v(i-1)^2*F(i)-9.81)*dt+v(i-1);
+    error = (v(i)-v(i-1))^2;
+    sim_time = sim_time+dt;
+    i = i+1;
+    
+end
+
+%disp(i)
+
+time = 0:dt:(length(v)-1)*dt;
+
+for i = 1:length(v)-1
+    grad(i) = (v(i+1)-v(i))/dt;
+    
+end
+
+figure
+plot(time,v)
+title('Descent Velocity After Parachute Deployment')
+xlabel('time / s')
+ylabel('Velocity / ms^{-1}')
+grid minor
+grid on
+
+figure
+plot(time(1:length(time)-1),m*grad)
+title(['Shock Load due to Parachute Deployment with t_{free} = ', num2str(t_delay), 's'])
+grid minor
+grid on
+text(total_time*0.3,max(m*grad)/2,['Max shock load = ', num2str(double(max(m*grad))), ' N'])
+text(total_time*0.3,max(m*grad)/2.4,['Average load = ', num2str(sum(m*grad)*dt/total_time), ' N'])
+xlabel('time / s')
+ylabel('Force / N')
+
+disp(['Total impulse delivered = ', num2str(sum(grad)*dt*m), ' Ns'])
+disp(['Max shock load = ', num2str(max(m*grad)), ' N'])
+disp(['Average load = ', num2str(sum(m*grad)*dt/total_time), ' N'])
